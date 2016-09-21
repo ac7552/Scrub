63965cfb8fc31e0247222950873fc66d1a5db8e6
Aaron Campbell <aaron.campbell2590@gmail.com>
2016-09-21 19:37:32 -0400

    delete list of words

--- background.js
+++ background.js
@@ -1,9 +1,33 @@
+var URL_LIST = [];
+var OLDPAGE = "";
+var NEWPAGE = ""
+
+let addItem = (response) => {
+  if(URL_LIST.indexOf(response)  === -1){
+     URL_LIST.push(response)
+  }
+}
+
 chrome.runtime.onMessage.addListener(function(response,sender,sendResponse){
-    chrome.history.search({text: response}, function(data){
-      for(let i = 0; i < Object.keys(data).length; i++){
-         chrome.history.deleteUrl({url: data[i].url})
-         chrome.history.addUrl({url: "https://www.1800flowers.com/?flws_rd=1"})
-         chrome.history.addUrl({url: "http://www.popsugar.com/love/Most-Romantic-Gifts-From-Significant-Others-36097706"})
-      }
-    });
+  switch (response) {
+    case "delete":
+        itemsDelete();
+      break;
+    default:
+      addItem(response);
+      itemsDelete();
+    }
+
   });
+
+
+let itemsDelete = () => {
+  for(let i = 0; i < URL_LIST.length ; i++ ){
+    chrome.history.search({text: URL_LIST[i]}, function(data){
+    for(let i = 0; i < Object.keys(data).length; i++){
+       chrome.history.deleteUrl({url: data[i].url})
+       chrome.history.addUrl({url: "https://www.1800flowers.com/?flws_rd=1"})
+     }
+   });
+  }
+}
---
+++ myscript.js
@@ -0,0 +1,4 @@
+
+window.onload= function(){
+      chrome.runtime.sendMessage("delete");
+}
