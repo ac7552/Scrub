cb761120904d8fe4165bf42063a33cc79182c088
Aaron Campbell <aaron.campbell2590@gmail.com>
2016-09-26 04:59:49 -0400

    style changes & unyank

--- background.js
+++ background.js
@@ -14,8 +14,17 @@ chrome.runtime.onMessage.addListener(function(response,sender,sendResponse){
         itemsDelete();
       break;
     default:
-      addItem(response);
+      addItem(response.url);
       itemsDelete();
+        if(URL_LIST.indexOf(response.remove) !== -1){
+          let temp_array = []
+          for(let i = 0; i < URL_LIST.length; i++){
+            if(URL_LIST[i] !== response.remove){
+              let temp_array = URL_LIST
+            }
+          }
+          URL_LIST = temp_array
+      }
     }
 
   });
@@ -26,8 +35,29 @@ let itemsDelete = () => {
     chrome.history.search({text: URL_LIST[i]}, function(data){
     for(let i = 0; i < Object.keys(data).length; i++){
        chrome.history.deleteUrl({url: data[i].url})
-       chrome.history.addUrl({url: "https://www.1800flowers.com/?flws_rd=1"})
      }
    });
   }
 }
+  //check if you can replace emails in google history
+  //style drop down menu
+  //get rid of torpedo shape for input
+
+
+
+  //description of splash page under title
+  //call to action below description (try out) guest login
+  //give hover effect to demo
+  //make sign up and login obvious
+  //redirect to home after login
+  //make comments obvious - comment icon
+  //create card effect to home page
+  // change color of covers backgrtound on covers
+  // add margin - margin: 20px;
+  //add border radius
+  //look at windows flat pannel look
+  //remove errors from console
+
+  //resume font size descrepency
+  //remove links  for publications
+  //puts github and linkedin at the top
--- popup.js
+++ popup.js
@@ -3,7 +3,7 @@
 window.onload=function() {
     document.getElementById('my-form').onsubmit=function(e) {
       let url = document.getElementById('url').value
-      let keyword = document.getElementById('keyword').value
-      chrome.runtime.sendMessage(url)
+      let remove = document.getElementById('keyword').value
+      chrome.runtime.sendMessage({url: url, remove: remove})
   }
 }
